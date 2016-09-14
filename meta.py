2b445b6167c1d9844e5330fb52bc29d2ef11b440
irrwitz <j.cyriac@gmail.com>
2016-09-14 18:15:47 +0200

    Initial commit

---
+++ meta.py
@@ -0,0 +1,21 @@
+import requests
+from flask import Flask, render_template, request
+
+# PAGING 
+START=0
+ROWS=100
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+@app.route('/search')
+def search():
+    searchterm = request.args.get('q')
+    url = 'http://localhost:8983/solr/pacs/query?wt=json&start=0&rows=500&q=StudyDescription:' + searchterm
+    response = requests.get(url)
+    data = response.json()
+    docs = data['response']['docs']
+    return render_template('table.html', docs=docs)
