6ca155e6ca305817877aaa467728ea52072c8cfc
Joshy Cyriac <j.cyriac@gmail.com>
2016-09-26 11:24:22 +0200

    Date picker should only start at 2010

--- meta/static/js/script.js
+++ meta/static/js/script.js
@@ -4,17 +4,17 @@ $(function () {
   var startDatePicker = $('#start-date-picker').pikaday({
     format: 'DD.MM.YYYY',
     firstDay: 1,
-    minDate: new Date(2000, 0, 1),
+    minDate: new Date(2010, 0, 1),
     maxDate: new Date(),
-    yearRange: [2000, 2016]
+    yearRange: [2010, 2016]
   });
 
   var endDatePicker = $('#end-date-picker').pikaday({
     format: 'DD.MM.YYYY',
     firstDay: 1,
-    minDate: new Date(2000, 0, 1),
+    minDate: new Date(2010, 0, 1),
     maxDate: new Date(),
-    yearRange: [2000, 2016]
+    yearRange: [2010, 2016]
   });
 
   $('#download-button').on('click', function(e) {
