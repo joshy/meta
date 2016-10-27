d733cb5ae3e51c5a44125df975d8990b360ee85a
Joshy Cyriac <j.cyriac@gmail.com>
2016-10-27 15:48:59 +0200

    Simple paging now works, need to have sliding window

--- meta/paging.py
+++ meta/paging.py
@@ -1,15 +1,14 @@
 import meta.settings
-import meta.query_param
+from meta.query_param import set_query_parameter
 
 
 def calc(results, url, current):
-    print(current)
     limit = meta.settings.RESULT_LIMIT
     pages = results // limit
     r = []
-    current_page = max(int(current) // limit, 1)
+    current_page = max(int(current) // limit, 0)
 
-    for x in range(1, min(pages + 1, 21)):
-        new_url = meta.query_param.set_query_parameter(url, 'offset', x * limit)
-        r.append((x, new_url, True if x == current_page else False))
+    for x in range(0, min(pages, 20)):
+        new_url = set_query_parameter(url, 'offset', x * limit)
+        r.append((x + 1, new_url, True if x == current_page else False))
     return r
--- tests/test_paging.py
+++ tests/test_paging.py
@@ -1,7 +1,12 @@
 import unittest
-import meta.query
+from meta.paging import calc
 
 
 class TestQueryStuff(unittest.TestCase):
-    def update_url(self):
-        self.assertTrue(True)
+    def test_update_url(self):
+        r = calc(200, 'http://localhost', '1')
+        self.assertEqual(0, len(r))
+
+    def test_update_url(self):
+        r = calc(700, 'http://localhost', '1')
+        self.assertEqual(1, len(r))
