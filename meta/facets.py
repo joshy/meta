b3082303c3b1c46aac694dd4267fbddae946a3a0
Joshy Cyriac <j.cyriac@gmail.com>
2016-11-01 10:04:33 +0100

    Facets links needs to reset offset , otherwise it could point to a empty result page

--- meta/facets.py
+++ meta/facets.py
@@ -1,4 +1,4 @@
-from meta.query_param import set_query_parameter
+from meta.query_param import set_query_parameter, delete_query_parameter
 
 
 def prepare_facets(facets, url):
@@ -11,6 +11,7 @@ def prepare_facets(facets, url):
                     for e in bucket_value:
                         value = '"{0}"'.format(e['val'])
                         new_url, clear = set_query_parameter(url, key, value)
+                        new_url = delete_query_parameter(new_url, 'offset')
                         e['url'] = new_url
                         e['clear'] = clear
 
--- meta/query_param.py
+++ meta/query_param.py
@@ -29,3 +29,19 @@ def set_query_parameter(url, param_name, param_value):
 
     return url, clear
 
+
+def delete_query_parameter(url, param_name):
+    """Given a URL, removes the query parameter and returns the
+    modified URL.
+
+    >>> delete_query_parameter('http://example.com?foo=bar', 'foo')
+    'http://example.com'
+
+    """
+    scheme, netloc, path, query_string, fragment = urlsplit(url)
+    query_params = parse_qs(query_string)
+    query_params.pop(param_name, None)
+    new_query_string = urlencode(query_params, doseq=True)
+    url = urlunsplit((scheme, netloc, path, new_query_string, fragment))
+
+    return url
