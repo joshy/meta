a8720d2e36ef24326f9b40601944376455c82e6f
Joshy Cyriac <j.cyriac@gmail.com>
2016-11-14 13:48:32 +0100

    Fixed logging configuration

--- runserver.py
+++ runserver.py
@@ -1,3 +1,6 @@
+"""
+Module to run the application. Defines also logging configuration.
+"""
 import os
 import logging
 from logging.handlers import TimedRotatingFileHandler
@@ -5,16 +8,17 @@ from logging.handlers import TimedRotatingFileHandler
 from meta import app
 
 
-LOG_DIR = 'logs'
+LOG_DIR = os.path.join(os.path.abspath(__file__), 'logs')
+
 if not os.path.exists(LOG_DIR):
     os.makedirs(LOG_DIR)
 
-handler = TimedRotatingFileHandler('logs/server.log', when='midnight',
-                                   backupCount=10)
-handler.setLevel(logging.DEBUG)
-formatter = logging.Formatter(
-    "%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-handler.setFormatter(formatter)
+HANDLER = TimedRotatingFileHandler(LOG_DIR, when='midnight', backupCount=10)
+HANDLER.setLevel(logging.DEBUG)
 
-app.logger.addHandler(handler)
+FORMATTER = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+HANDLER.setFormatter(FORMATTER)
+
+app.logger.addHandler(HANDLER)
 app.run(debug=True, host='0.0.0.0')
