516d8f71d6ce9f8f038c1723ccc38b47ad278a96
Joshy Cyriac <j.cyriac@gmail.com>
2016-11-23 10:08:59 +0100

    Removed prints

--- meta/app.py
+++ meta/app.py
@@ -2,9 +2,7 @@ from flask import Flask
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('meta.default_config')
-print(app.config['DEBUG'])
 app.config.from_pyfile('config.cfg')
-print(app.config['DEBUG'])
 app.config['VERSION'] = '1.0.0'
 
 import meta.views
