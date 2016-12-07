344b09177c15e8a0a065ff9bfdab1c82724414b2
Joshy Cyriac <j.cyriac@gmail.com>
2016-12-07 09:07:53 +0100

    Increased also patch version

--- meta/app.py
+++ meta/app.py
@@ -3,6 +3,6 @@ from flask import Flask
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('meta.default_config')
 app.config.from_pyfile('config.cfg')
-app.config['VERSION'] = '1.1.0'
+app.config['VERSION'] = '1.1.1'
 
 import meta.views
