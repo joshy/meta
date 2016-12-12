95e7fe449aed6372435d126e6809e21748f9618d
Joshy Cyriac <j.cyriac@gmail.com>
2016-12-12 11:21:12 +0100

    Fixed test for now, should use PAGING from settings

--- tests/test_paging.py
+++ tests/test_paging.py
@@ -5,9 +5,9 @@ from meta.paging import calc
 class TestQueryStuff(unittest.TestCase):
     def test_update_url1(self):
         r = calc(200, 'http://localhost', '1')
-        self.assertEqual(0, len(r))
+        self.assertEqual(2, len(r))
 
     def test_update_url2(self):
         r = calc(1200, 'http://localhost', '1')
-        self.assertEqual('http://localhost?offset=500', r[1][1])
-        self.assertEqual(3, len(r))
+        self.assertEqual('http://localhost?offset=100', r[1][1])
+        self.assertEqual(12, len(r))
