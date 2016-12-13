171c8dbecc6f054433209f3427c9c4bb4249d4be
Joshy Cyriac <j.cyriac@gmail.com>
2016-12-13 22:18:28 +0100

    Using Flask-Assets

--- meta/app.py
+++ meta/app.py
@@ -1,8 +1,17 @@
 from flask import Flask
+from flask_assets import Environment, Bundle
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('meta.default_config')
 app.config.from_pyfile('config.cfg')
 app.config['VERSION'] = '1.1.3'
+
+assets = Environment(app)
+js = Bundle("js/jquery-3.1.0.min.js", "js/tether.min.js",
+            "js/bootstrap.min.js", "js/moment.min.js", "js/pikaday.js",
+            "js/pikaday.jquery.js", "js/jquery.noty.packaged.min.js",
+            "js/script.js",
+            filters='jsmin', output='gen/packed.js')
+assets.register('js_all', js)
 
 import meta.views
