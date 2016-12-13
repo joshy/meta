4b1fd7fc1c1a56a4b772f678148bf861d9b55884
Joshy Cyriac <j.cyriac@gmail.com>
2016-12-13 17:29:04 +0100

    Increased minor version

--- meta/app.py
+++ meta/app.py
@@ -3,6 +3,6 @@ from flask import Flask
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('meta.default_config')
 app.config.from_pyfile('config.cfg')
-app.config['VERSION'] = '1.1.2'
+app.config['VERSION'] = '1.1.3'
 
 import meta.views
