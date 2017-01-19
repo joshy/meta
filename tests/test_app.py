42a4612538b4393acafb8d1f18c815570cd27cc0
Joshy Cyriac <j.cyriac@gmail.com>
2017-01-19 14:55:21 +0100

    Fixed test again, come on travis

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,9 +1,7 @@
 import unittest
-from meta.app import CONNECTION
-
+from meta.app import app, CONNECTION
 
 class TestApp(unittest.TestCase):
-
     def test_cmd(self):
         expected = '-aet YETI -aec AE_CALLED 127.0.0.1 104 +P 11110 /Applications/dcmtk/dcm.in'
         self.assertEqual(expected, CONNECTION)
--- tests/test_command.py
+++ tests/test_command.py
@@ -3,7 +3,7 @@ from meta.command import base_command, transfer_command
 from meta.app import app
 
 class TestCommand(unittest.TestCase):
-    app.config.from_pyfile('test_config.cfg', silent=True)
+
     def test_base_command(self):
         expected = '/Applications/dcmtk/dcmtk-3.6.0-mac-i686-dynamic/bin/movescu -v -S -k QueryRetrieveLevel=SERIES -aet YETI -aec AE_CALLED 127.0.0.1 104 +P 11110 /Applications/dcmtk/dcm.in'
         self.assertEqual(expected, base_command())
