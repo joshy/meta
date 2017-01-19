fb6628e481d3da9c6baaf66be9fc9b342a232584
Joshy Cyriac <j.cyriac@gmail.com>
2017-01-19 11:39:02 +0100

    Write to file when not in debug mode

--- runserver.py
+++ runserver.py
@@ -7,19 +7,19 @@ from logging.handlers import TimedRotatingFileHandler
 
 from meta.app import app
 
+if not app.debug:
+    LOG_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'logs')
+    LOG_FILE = os.path.join(LOG_DIR, 'server.log')
 
-LOG_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'logs')
-LOG_FILE = os.path.join(LOG_DIR, 'server.log')
+    if not os.path.isdir(LOG_DIR):
+        os.makedirs(LOG_DIR)
 
-if not os.path.isdir(LOG_DIR):
-    os.makedirs(LOG_DIR)
+    HANDLER = TimedRotatingFileHandler(LOG_FILE, when='midnight', backupCount=10)
+    HANDLER.setLevel(logging.DEBUG)
 
-HANDLER = TimedRotatingFileHandler(LOG_FILE, when='midnight', backupCount=10)
-HANDLER.setLevel(logging.DEBUG)
-
-FORMATTER = logging.Formatter(
-    "%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-HANDLER.setFormatter(FORMATTER)
-app.logger.addHandler(HANDLER)
+    FORMATTER = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    HANDLER.setFormatter(FORMATTER)
+    app.logger.addHandler(HANDLER)
 
 app.run(host='0.0.0.0')
