3471c5e0b8d24a1f68a1b46979dd93474390ade9
Joshy Cyriac <j.cyriac@gmail.com>
2017-01-20 20:52:42 +0100

    fixed on test

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,5 +3,5 @@ from meta.app import app, CONNECTION
 
 class TestApp(unittest.TestCase):
     def test_cmd(self):
-        expected = '-aet AE_TITLE -aec AE_CALLED 127.0.0.1 104 +P 11110 /Applications/dcmtk/dcm.in'
+        expected = '-aet AE_TITLE -aec AE_CALLED 127.0.0.1 104 +P 11110'
         self.assertEqual(expected, CONNECTION)
