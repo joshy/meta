369d50edd7fa76c6fc0513add083d31e6df562b3
Jessica Odermatt <jessica.odermatt@students.fhnw.ch>
2017-06-25 01:16:16 +0200

    template changes

--- meta/static/js/upload.js
+++ meta/static/js/upload.js
@@ -1,22 +1,17 @@
 /**
- * Created by Yannick on 12.06.2017.
+ * Created by Yannick on 12.06.2017
  */
 
-var inputData = {"patients": [ { "first_name": "Bram", "last_name": "L. Munich", "birthdate": "24.01.1964" }, { "first_name": "Flavio", "last_name": "D. Konstanz", "birthdate": "09.07.1966" } ]}
+
+var inputData = {"patients": [ { "first_name": "Bram", "last_name": "^L.^Munich", "birthdate": "24.01.1964" } ]}
 // call to backend
 function getData() {
-    alert("getData");
     $.ajax({
         type: "POST",
         url: "query_patients",
         data: JSON.stringify(inputData),
         success: function(d) {
             console.log(d)
-            alert(d.groups);
-            alert(d.ngroups);
-            alert(d.matches);
-            alert(d);
-
         },
         contentType: 'application/json',
         dataType: 'json'
