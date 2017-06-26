bfd2bcf4b004e4c41e80df34f0c76609f3657f3a
Jessica Odermatt <jessica.odermatt@students.fhnw.ch>
2017-06-26 15:02:45 +0200

    make data a global var

--- meta/static/js/fileupload/prototyp.js
+++ meta/static/js/fileupload/prototyp.js
@@ -5,6 +5,7 @@
 
 ================================================================================ */
 
+/* define global variables */
 var fileControl = false;
 var parseControl = false;
 var resetControl = false;
@@ -12,10 +13,10 @@ var rowHeaderControl = false;
 var previewControl = false;
 var dropareaControl = false;
 var sourceFile = false;
-var data = false;
+var fileData = false;
+var searchData = false;
 
 /* define valid date formats for birthdate detection* */
-
 var birthdayFormats = [
     'YYYYMMDD',
     'YYYY-MM-DD',
@@ -23,7 +24,6 @@ var birthdayFormats = [
     'DD.MM.YYYY',
     'DD.MM.YY'
 ]
-
 
 /* define auto matiching mechanics */
 var autoMatches = [];
@@ -82,7 +82,7 @@ function CreateTable(data, amount) {
     }
 
     var table = document.createElement('table');
-    table.className = "table-responsive";
+    table.className = "table-responsive table-bordered";
     for (var i = 0; i < amount; i++) {
         var row = document.createElement('tr');
         if (data[i]) {
@@ -100,6 +100,7 @@ function CreateTable(data, amount) {
 function Reset() {
     sourceFile = false;
     previewControl.html('');
+    rowHeaderControl.closest('.firstRowIsHeader').hide();
     console.clear();
 }
 
@@ -129,18 +130,18 @@ function CheckFile() {
         case 'xls':
         case 'xlsx':
             parseControl.prop('disabled', false);
+            rowHeaderControl.closest('.firstRowIsHeader').show();
             fileControl.parent('form').removeClass('invalid');
             result = true;
             break;
         default:
             parseControl.prop('disabled', true);
+            rowHeaderControl.closest('.firstRowIsHeader').hide();
             fileControl.parent('form').addClass('invalid');
             break;
     }
     return result;
 }
-
-
 
 /* Get File content (header + content) based on file extension */
 function GetFileContent(callback) {
@@ -193,24 +194,85 @@ function WorkbookToJson(workbook) {
 function PreviewFileContent(data) {
     var table = CreateTable(data, 5);
     previewControl.html(table);
+    fileData = data;
 }
 
 /* Prepare Content for next step */
-function PrepareFileContent(data) {
-    data = SplitData(data);
-    data = DetectBirthdate(data);
-    data = PrepareDefaults(data);
-    data = fillBoxes(data);
-    //console.log(JSON.stringify(data, 2, 2));
-    console.log(JSON.stringify(data, 2, 2));
+function PrepareFileContent() {
+    SplitData();
+    DetectBirthdate();
+    PrepareDefaults();
+    fillBoxes();
+    //console.log(JSON.stringify(fileData, 2, 2));
+}
+
+/* fill markup of boxes for structure recognition */
+function fillBoxes() {
+    var parent = $('.step-2 .data-list');
+    var box = parent.find('.data-template > div');
+
+    $.each(fileData['header'], function(key, value) {
+        var tempBox = box.clone(true);
+        tempBox.addClass("box box-" + key).removeClass('data-template');
+        tempBox.data('id', key);
+
+        tempBox.find('[data-tmpl="header"]').html(value.title);
+        tempBox.find('[data-tmpl="sampledata"]').html(value.sampledata);
+
+        if (value.selected) {
+            tempBox.find('[data-tmpl="select"]').val(value.selected);
+            tempBox.find('.card-header').removeClass('has-warning').addClass('has-success');
+        }
+
+        tempBox.appendTo(parent);
+    });
+
+    // bind change event to all boxes
+    $(window).on('change', box, function(event) {
+        changeSelectedData(event.target);
+    });
+
+    goToStep(2);
+    return;
+}
+
+/* change selected values */
+function changeSelectedData(select) {
+    // change views
+    var $select = $(select);
+    var box = $(select).closest('.box');
+    var selectedValue = $select.val();
+
+    $('.data-list .box select').each(function() {
+        if ($(this).val() != "" && $(this).val() == selectedValue) {
+            $(this).val("");
+            $(this).closest('.card-header').removeClass('has-success').addClass('has-warning');
+        }
+    });
+    $select.val(selectedValue);
+    console.log(box);
+    box.find('.card-header').removeClass('has-warning').addClass('has-success')
+
+    //change data in array
+    $.each(fileData['header'], function (key, value) {
+        if (value['selected'] && value['selected'] == selectedValue) {
+            value['selected'] = "";
+        }
+        if (key == box.data('id')) {
+            value['selected'] = selectedValue;
+        }
+    });
+
+    console.log(JSON.stringify(fileData, 2, 2));
+    return;
 }
 
 /* prepare data to send */
-function FinalizeData(data) {
+function FinalizeData() {
     var result = [];
 
     // prefill empty data array
-    $.each(data['content'], function (key, value) {
+    $.each(fileData['content'], function (key, value) {
         result.push({
             'full_name': '',
             'birthdate': '',
@@ -219,27 +281,27 @@ function FinalizeData(data) {
     });
 
     // fill data array based on selected cols
-    $.each(data['header'], function (key, value) {
+    $.each(fileData['header'], function (key, value) {
         if (value['selected']) {
             switch (value['selected']) {
                 case 'firstname':
-                    $.each(data['content'], function (subkey, subvalue) {
+                    $.each(fileData['content'], function (subkey, subvalue) {
                         result[subkey]['full_name'] = subvalue[key] + result[subkey]['full_name'];
                     });
                     break;
                 case 'lastname':
-                    $.each(data['content'], function (subkey, subvalue) {
+                    $.each(fileData['content'], function (subkey, subvalue) {
                         result[subkey]['full_name'] = result[subkey]['full_name'] + subvalue[key];
                     });
                     break;
                 case 'full_name':
-                    $.each(data['content'], function (subkey, subvalue) {
+                    $.each(fileData['content'], function (subkey, subvalue) {
                         result[subkey]['full_name'] = subvalue[key];
                     });
 
                     break;
                 case 'birthdate':
-                    $.each(data['content'], function (subkey, subvalue) {
+                    $.each(fileData['content'], function (subkey, subvalue) {
                         $.each(birthdayFormats, function (key, value) {
                             var parsedDate = moment(subvalue[key], value, true);
                             if (parsedDate.isValid()) {
@@ -250,7 +312,7 @@ function FinalizeData(data) {
 
                     break;
                 case 'patient_id':
-                    $.each(data['content'], function (subkey, subvalue) {
+                    $.each(fileData['content'], function (subkey, subvalue) {
                         result[subkey]['patient_id'] = subvalue[key];
                     });
 
@@ -261,63 +323,59 @@ function FinalizeData(data) {
     });
 
     result = { patients: result };
-    return result;
+    fileData = result;
 }
 
 /* Prepare default dropdown values */
-function PrepareDefaults(data) {
+function PrepareDefaults() {
     var birthdateUsed = false;
-    $.each(data['header'], function (key, value) {
+    $.each(fileData['header'], function (key, value) {
         if (value['autoMatchType']) {
-            data['header'][key]['selected'] = value['autoMatchType'];
+            fileData['header'][key]['selected'] = value['autoMatchType'];
             if (value['autoMatchType'] == 'birthdate') {
                 birthdateUsed = true;
             }
         }
-        data['header'][key]['sampledata'] = data['header'][key]['sampledata'].join(',');
+        fileData['header'][key]['sampledata'] = fileData['header'][key]['sampledata'].join(',');
     });
 
     if (!birthdateUsed) {
-        $.each(data['header'], function (key, value) {
+        $.each(fileData['header'], function (key, value) {
             if (value['possibleBirthdate']) {
-                data['header'][key]['selected'] = 'birthdate';
+                fileData['header'][key]['selected'] = 'birthdate';
                 return false;
             }
         });
     }
-
-    return data;
 }
 
 /* auto detect birthdate based on values */
-function DetectBirthdate(data) {
-    if (data['content'].length > 0) {
-        var firstDataLine = data['content'][0];
+function DetectBirthdate() {
+    if (fileData['content'].length > 0) {
+        var firstDataLine = fileData['content'][0];
 
         for (var i = 0; i < firstDataLine.length; i++) {
             $.each(birthdayFormats, function (key, value) {
                 var parsedDate = moment(firstDataLine[i], value, true);
                 if (parsedDate.isValid()) {
-                    data['header'][i]['possibleBirthdate'] = true;
+                    fileData['header'][i]['possibleBirthdate'] = true;
                 }
             });
         }
     }
-
-    return data;
 }
 
 /* prepare data for manipulation */
-function SplitData(data) {
+function SplitData() {
     var result = { 'header': [], 'content': [] };
     var maxColSize = 0;
 
-    for (var i = 0; i < data.length; i++) {
+    for (var i = 0; i < fileData.length; i++) {
         if (rowHeaderControl.is(':checked') && i == 0) {
             var headerData = [];
-            for (var j = 0; j < data[i].length; j++) {
+            for (var j = 0; j < fileData[i].length; j++) {
                 var headerItem = {};
-                var title = data[i][j];
+                var title = fileData[i][j];
                 headerItem['title'] = title;
                 headerItem['sampledata'] = [];
                 title = $.trim(title.toLowerCase());
@@ -334,10 +392,10 @@ function SplitData(data) {
             }
         }
         else {
-            if (data[i].length > maxColSize) {
-                maxColSize = data[i].length;
+            if (fileData[i].length > maxColSize) {
+                maxColSize = fileData[i].length;
             }
-            result['content'].push(data[i]);
+            result['content'].push(fileData[i]);
         }
     }
 
@@ -362,7 +420,7 @@ function SplitData(data) {
         }
     }
 
-    return result;
+    fileData = result;
 }
 
 /* ================================================================================
@@ -373,13 +431,14 @@ $(function () {
     /* define controls */
     fileControl = $('#uploadFile');
     parseControl = $('#btnParse');
-    resetControl = $('#btnReset');
+    resetControl = $('.step-1 [data-reset], [data-dismiss="modal"]');
     rowHeaderControl = $('#firstRowIsHeader');
     previewControl = $('#filePreview');
     dropareaControl = $('#droparea');
 
-    /* init */
+    /* init file upload & file content recognition */
     parseControl.prop('disabled', true);
+    rowHeaderControl.closest('.firstRowIsHeader').hide();
 
     rowHeaderControl.on('change', function () {
         if (CheckFile()) {
@@ -395,10 +454,6 @@ $(function () {
         Reset();
     });
 
-    // sendControl.on('click', function() {
-    //     FinalizeData();
-    // })
-
     dropareaControl.on('click', function () {
         $(this).find('input [type=file]').click();
     });
@@ -410,31 +465,36 @@ $(function () {
 
             if (CheckFile()) {
                 GetFileContent(PreviewFileContent);
+                console.log(fileData);
             };
         })
+
     })
 
-    $(window).on('change', '.data-list .data-template', function(event) {
-        changeSelectedData(event.target);
-    });
+    /* init structure  */
+    
+
+    // sendControl.on('click', function() {
+    //     FinalizeData();
+    // })
 });
 
 
 /* ================================================================================
-    Step Helper
+    Progress Step Helper
 ================================================================================ */
 
 /* show/hide loading button */
 function toggleLoader(nextStep) {
-    var loaderBtn = $('.btn.step-' + (nextStep-1));
+    // var loaderBtn = $('.btn.step-' + (nextStep-1));
 
-    if (loaderBtn.hasClass('loading')) {
-        loaderBtn.removeClass('loading');
-        loaderBtn.prop("disabled", false);
-    } else {
-        loaderBtn.addClass('loading');
-        loaderBtn.prop("disabled", true);
-    }
+    // if (loaderBtn.hasClass('loading')) {
+    //     loaderBtn.removeClass('loading');
+    //     loaderBtn.prop("disabled", false);
+    // } else {
+    //     loaderBtn.addClass('loading');
+    //     loaderBtn.prop("disabled", true);
+    // }
 }
 
 /* goes to a specific step */
@@ -454,56 +514,4 @@ function searchPatients(data) {
         contentType: 'application/json',
         dataType: 'json'
     });
-}
-
-function fillBoxes(data) {
-    toggleLoader(2);
-
-    var parent = $('.step-2 .data-list');
-    var box = parent.find('.data-template > div');
-
-    $.each(data['header'], function(key, value) {
-        var tempBox = box.clone(true);
-        tempBox.addClass("box box-" + key).removeClass('data-template');
-        tempBox.data('id', key);
-
-        tempBox.find('[data-tmpl="header"]').html(value.title);
-        tempBox.find('[data-tmpl="sampledata"]').html(value.sampledata);
-        tempBox.find('[data-tmpl="select"]').val(value.selected);
-        tempBox.appendTo(parent);
-    });
-
-    goToStep(2);
-    return data;
-}
-
-/* change selected values */
-function changeSelectedData(select) {
-    // change views
-    var $select = $(select);
-    var box = $(select).closest('.box');
-    var selectValue = $select.val();
-
-    $('.box select').each(function() {
-        if ($(this).val() == $select.val()) {
-            $(this).val("");
-            console.log($(this).closest('.card-header'));
-            $(this).closest('.card-header').removeClass('has-success').addClass('has-warning');
-        }
-    });
-
-    $select.val(selectValue);
-    box.removeClass('has-warning').addClass('has-success');
-
-    // change data array
-    $.each(data['header'], function (key, value) {
-        if (value['selected'] && value['selected'] == selectValue) {
-            value['selected'] = "";
-        }
-        if (key == box.data('id')) {
-            value['selected'] = selectValue;
-        }
-    });
-
-    console.log(data);
 }
