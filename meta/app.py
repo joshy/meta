a3ac503d1c64628340712a1ee47030f339497fb4
Joshy Cyriac <j.cyriac@gmail.com>
2017-07-05 20:36:08 +0200

    increase minor version

--- meta/app.py
+++ meta/app.py
@@ -11,7 +11,7 @@ app.config.from_pyfile('config.cfg', silent=True)
 
 # Exposing constants to use
 DEMO = app.config['DEMO']
-VERSION = app.config['VERSION'] = '1.4.1'
+VERSION = app.config['VERSION'] = '1.4.2'
 RESULT_LIMIT = app.config['RESULT_LIMIT']
 
 # DCMTK settings
