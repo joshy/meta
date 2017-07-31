6daab2bdd14a60b142328b345c22e3e1db733fd3
Joshy Cyriac <j.cyriac@gmail.com>
2017-07-31 17:09:58 +0200

    one more test

--- tests/test_grouping.py
+++ tests/test_grouping.py
@@ -1,17 +1,26 @@
 import unittest
 from meta.grouping import group
 
-
-class TestGroupingStuff(unittest.TestCase):
-    def test_grouping(self):
-        result = group(TEST_DATA)
-        # same group size as before
-        groups = result['groups']
-        self.assertEqual(2, len(groups))
-        # has grouped docs
-        self.assertTrue('by_AccessionNumber' in groups[0])
-        self.assertTrue('by_AccessionNumber' in groups[1])
-
+TEST_DATA_1 = {'groups': [
+        {'groupValue': 'ABC',
+        'doclist': {'numFound': 1, 'start': 0,
+                    'docs': [
+                        {
+                            'AccessionNumber': '1',
+                            'StudyID': '1',
+                        }
+                    ]}
+        },
+        {
+            'groupValue': 'DEF',
+            'doclist': {'numFound': 1, 'start': 0,
+                        'docs': [
+                            {
+                                'AccessionNumber': '3',
+                                'StudyID': '20725498'}
+                        ]}
+        }
+    ]}
 
 TEST_DATA = {'groups': [
     {'groupValue': 'ABC',
@@ -47,3 +56,30 @@ TEST_DATA = {'groups': [
                         }
                     ]}
     }]}
+
+
+
+class TestGroupingStuff(unittest.TestCase):
+
+
+    def test_grouping1(self):
+        result = group(TEST_DATA_1)
+        # same group size as before
+        groups = result['groups']
+        self.assertEqual(2, len(groups))
+        # has grouped docs
+        self.assertTrue('by_AccessionNumber' in groups[0])
+        self.assertTrue('by_AccessionNumber' in groups[1])
+
+
+    def test_grouping(self):
+        result = group(TEST_DATA)
+        # same group size as before
+        groups = result['groups']
+        self.assertEqual(2, len(groups))
+        # has grouped docs
+        self.assertTrue('by_AccessionNumber' in groups[0])
+        self.assertTrue('by_AccessionNumber' in groups[1])
+
+
+
