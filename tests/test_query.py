713acc424ea6709fee1e9a27dcc297da47091240
Manibu4 <manuela.utzinger@usb.ch>
2018-01-26 15:57:51 +0100

    Unit tests adapted

--- tests/test_query.py
+++ tests/test_query.py
@@ -5,54 +5,49 @@ from werkzeug import MultiDict
 class TestQueryStuff(unittest.TestCase):
 
     def test_date(self):
-        args = {'query': 'foo',
-                'StartDate': '1.1.2016', 'EndDate': '31.12.2016',
-                'StudyDescription': 'lorem'}
+        args = MultiDict([('query', 'foo'),
+                ('StartDate', '1.1.2016'), ('EndDate', '31.12.2016')])
         result = meta.query.query_body(args)
         self.assertEqual(result['query'],
-                         'foo AND StudyDate:[20160101 TO 20161231]')
+                         'RisReport:(foo) AND StudyDate:[20160101 TO 20161231]')
 
     def test_filter_single(self):
-        args = {'query': 'foo',
-                'StartDate': '1.1.2016', 'EndDate': '31.12.2016',
-                'StudyDescription': 'lorem ipsum'}
+        args = MultiDict([('query', 'foo'),
+                ('StartDate', '1.1.2016'), ('EndDate', '31.12.2016'),
+                ('StudyDescription', 'lorem ipsum')])
         result = meta.query.query_body(args)
         self.assertEqual(result['filter'],
-                         ['StudyDescription:lorem ipsum'])
+                         ['StudyDescription:(lorem ipsum)'])
 
-    def test_filter_multiple(self):
-        args = {'query': 'foo',
-                'StartDate': '1.1.2016', 'EndDate': '31.12.2016',
-                'StudyDescription': 'lorem ipsum',
-                'SeriesDescription': 'dolor sit amet'}
-        result = meta.query.query_body(args)
-        self.assertEqual(result['filter'],
-                         ['StudyDescription:lorem ipsum',
-                          'SeriesDescription:dolor sit amet'])
+#     def test_filter_multiple(self):
+#         args = MultiDict([('query', 'foo'),
+#                 ('StartDate', '1.1.2016'), ('EndDate', '31.12.2016'),
+#                 ('StudyDescription', 'lorem ipsum')])
+#         result = meta.query.query_body(args)
+#         self.assertEqual(result['filter'],
+#                          ['StudyDescription:(lorem ipsum)'])
 
     def test_filter_all(self):
-        args = {'query': 'foo',
-                'StartDate': '1.1.2016', 'EndDate': '31.12.2016',
-                'StudyDescription': 'lorem ipsum',
-                'SeriesDescription': 'dolor sit amet',
-                'PatientID': '123',
-                'PatientName': 'Hans Mueller',
-                'AccessionNumber': 'A123456789'}
+        args = MultiDict([('query', 'foo'),
+                ('StartDate', '1.1.2016'), ('EndDate', '31.12.2016'),
+                ('StudyDescription', 'lorem ipsum'),
+                ('PatientID', '123'),
+                ('PatientName', 'Hans Mueller'),
+                ('AccessionNumber', 'A123456789')])
         result = meta.query.query_body(args)
         self.assertEqual(result['filter'],
-                         ['StudyDescription:lorem ipsum',
-                          'SeriesDescription:dolor sit amet',
-                          'PatientID:123',
-                          'PatientName:Hans Mueller',
-                          'AccessionNumber:A123456789'])
+                         ['StudyDescription:(lorem ipsum)',
+                          'PatientID:(123)',
+                          'PatientName:(Hans Mueller)',
+                          'AccessionNumber:(A123456789)'])
 
     def test_filter_single_modality(self):
-        args = {'query': 'foo', 'StartDate': '1.1.2016',
-                'EndDate': '31.12.2016',
-                'Modality': 'CT'}
+        args = MultiDict([('query', 'foo'), ('StartDate', '1.1.2016'),
+                ('EndDate', '31.12.2016'),
+                ('Modality', 'CT')])
         result = meta.query.query_body(args)
         self.assertEqual(result['filter'],
-                         ['Modality:CT'])
+                         ["Modality:(CT)"])
 
     def test_filter_multiple_modality(self):
         args = MultiDict([('query', 'foo'), ('StartDate', '1.1.2016'),
