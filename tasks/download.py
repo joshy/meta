3a6ad9d2467f7902c6e1e3bf9802be6a7c8abc7d
Ivan Nešić <fonposta@gmail.com>
2018-02-23 13:43:06 +0100

    Created, fixed and removed obsolete tests.

--- tasks/download.py
+++ tasks/download.py
@@ -8,33 +8,37 @@ import subprocess
 
 class DownloadTask(luigi.contrib.external_program.ExternalProgramTask):
     # loaded from cfg
-    dcmin = luigi.Parameter()
-    movescu = luigi.Parameter()
-    aet = luigi.Parameter()
-    aec = luigi.Parameter()
-    peer_address = luigi.Parameter()
-    peer_port = luigi.Parameter()
-    incoming_port = luigi.Parameter()
-    output_dir = luigi.Parameter()
+    dcmin = luigi.Parameter(default='')
+    movescu = luigi.Parameter(default='')
+    aet = luigi.Parameter(default='')
+    aec = luigi.Parameter(default='')
+    peer_address = luigi.Parameter(default='')
+    peer_port = luigi.Parameter(default='')
+    incoming_port = luigi.Parameter(default='')
+    output_dir = luigi.Parameter(default='')
 
-    # luigi input args
-    patient_id = luigi.Parameter()
-    accession_number = luigi.Parameter()
-    series_number = luigi.Parameter()
-
-    study_instance_uid = luigi.Parameter()
-    series_instance_uid = luigi.Parameter()
-
-    dir_name = luigi.Parameter()
+    # luigi inputs
+    patient_id = luigi.Parameter(default='')
+    accession_number = luigi.Parameter(default='')
+    series_number = luigi.Parameter(default='')
+    study_instance_uid = luigi.Parameter(default='')
+    series_instance_uid = luigi.Parameter(default='')
+    dir_name = luigi.Parameter(default='')
 
     resources = {'pacs': 1}
     max_batch_size = 1
 
     def get_image_dir(self):
-        return join(self.output_dir, self.dir_name, self.patient_id, self.accession_number, self.series_number)
+        return join(
+            self.output_dir,
+            self.dir_name,
+            self.patient_id,
+            self.accession_number,
+            self.series_number
+        )
 
-    def program_args(self):
-        base_command = (
+    def get_base_command(self):
+        return (
             '{} -v -S -k QueryRetrieveLevel=SERIES -aet {} -aec {} {} {} +P {}'
             .format(
                 self.movescu,
@@ -46,15 +50,23 @@ class DownloadTask(luigi.contrib.external_program.ExternalProgramTask):
             )
         )
 
-        image_dir = self.get_image_dir()
-        if not exists(image_dir):
-            makedirs(image_dir, exist_ok=True)
-
-        command = (
+    def get_command(self, base_command, image_dir):
+        return (
             base_command +
             ' --output-directory ' + image_dir +
             ' -k StudyInstanceUID=' + self.study_instance_uid +
             ' -k SeriesInstanceUID=' + self.series_instance_uid +
             ' ' + self.dcmin
         )
+
+    def program_args(self):
+        base_command = self.get_base_command()
+
+        image_dir = self.get_image_dir()
+
+        if not exists(image_dir):
+            makedirs(image_dir, exist_ok=True)
+
+        command = self.get_command(base_command, image_dir)
+
         return shlex.split(command)
--- tests/test_command.py
+++ tests/test_command.py
@@ -1,6 +1,8 @@
 import unittest
-from meta.command_creator import _get_base_command, construct_transfer_command
+from meta.command_creator import construct_transfer_command
+from tasks.download import DownloadTask
 from meta.config import dcmtk_config, pacs_config
+from unittest.mock import patch
 
 DCMTK_CONFIG = dcmtk_config({'DCMTK_BIN':'/usr/local/bin/',
                              'DCMIN': '/opt/dcm.in'})
@@ -12,9 +14,63 @@ PACS_CONFIG = pacs_config({'AE_TITLE':'AE_TITLE', 'AE_CALLED': 'AE_CALLED',
 
 class TestCommand(unittest.TestCase):
     def test_base_command(self):
-        expected = '/usr/local/bin/movescu -v -S -k QueryRetrieveLevel=SERIES -aet AE_TITLE -aec AE_CALLED 127.0.0.1 104 +P 11110'
-        self.assertEqual(expected, _get_base_command(DCMTK_CONFIG, PACS_CONFIG))
+        expected = (
+            '/usr/local/bin/movescu '
+            '-v -S -k QueryRetrieveLevel=SERIES '
+            '-aet AE_TITLE '
+            '-aec AE_CALLED '
+            '127.0.0.1 104 +P 11110'
+        )
+
+        dt = DownloadTask()
+
+        dt.movescu = '/usr/local/bin/movescu'
+        dt.aet = 'AE_TITLE'
+        dt.aec = 'AE_CALLED'
+        dt.peer_address = '127.0.0.1'
+        dt.peer_port = '104'
+        dt.incoming_port = '11110'
+
+        self.assertEqual(expected, dt.get_base_command())
+
+    def test_image_dir(self):
+        expected = '/output_dir/dir_name/patient_id/accession_number/series_number'
+
+        dt = DownloadTask()
+
+        dt.output_dir = '/output_dir'
+        dt.dir_name = 'dir_name'
+        dt.patient_id = 'patient_id'
+        dt.accession_number = 'accession_number'
+        dt.series_number = 'series_number'
+
+        self.assertEqual(expected, dt.get_image_dir())
+
+    def test_get_command(self):
+        with patch('tasks.download.makedirs') as mock_makedirs:
+            mock_makedirs.return_value = None
+
+            dt = DownloadTask()
+
+            dt.study_instance_uid = 'study_instance_uid'
+            dt.series_instance_uid  = 'series_instance_uid'
+            dt.dcmin = 'dcmin'
+
+            expected = ('base_command '
+                        '--output-directory image_dir '
+                        '-k StudyInstanceUID=study_instance_uid '
+                        '-k SeriesInstanceUID=series_instance_uid dcmin')
+
+            self.assertEqual(expected, dt.get_command('base_command', 'image_dir'))
 
     def test_transfer(self):
-        expected = '/usr/local/bin/movescu -v -S -aem SRSYVMS01 -aet AE_TITLE -aec AE_CALLED 127.0.0.1 104 +P 11110 -k StudyInstanceUID=12345 /opt/dcm.in'
+        expected = (
+            '/usr/local/bin/movescu '
+            '-v -S -aem SRSYVMS01 '
+            '-aet AE_TITLE '
+            '-aec AE_CALLED '
+            '127.0.0.1 104 +P 11110 '
+            '-k StudyInstanceUID=12345 '
+            '/opt/dcm.in'
+        )
         self.assertEqual(expected, construct_transfer_command(DCMTK_CONFIG, PACS_CONFIG, 'syngo', '12345'))
