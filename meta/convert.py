c090f20708d40fd38eb1504495d5e70e3dd9e983
Joshy Cyriac <j.cyriac@gmail.com>
2019-05-22 17:09:26 +0200

    null safe get, default value is always evaluated!

--- meta/convert.py
+++ meta/convert.py
@@ -4,18 +4,18 @@ def convert(df):
     for _, row in df.iterrows():
         parent = {
             "patient_id": row.PatientID,
-            "patient_birth_date":row.PatientBirthDate,
-            "study_description":row.StudyDescription,
+            "patient_birth_date": row.PatientBirthDate,
+            "study_description": row.StudyDescription,
             "study_date": row.StudyDate,
-            "accession_number":row.AccessionNumber,
-            "study_uid": row.get("StudyInstanceUID", "") # newer ones have none!
+            "accession_number": row.AccessionNumber,
+            "study_uid": row.get("StudyInstanceUID"),  # newer ones have none!
         }
         for s in row["_childDocuments_"]:
             p = parent.copy()
             # Important: Newer versions of the crawler has the StudyInstanceUID
             # on the series level (because of GRASP e.g.) Now if the series level
             # is empty it should  use the one from the study level
-            p["study_uid"] = s.get("StudyInstanceUID", row.StudyInstanceUID)
+            p["study_uid"] = s.get("StudyInstanceUID")
             p["series_uid"] = s["SeriesInstanceUID"]
             p["series_description"] = s.get("SeriesDescription", "")
             p["series_number"] = s.get("SeriesNumber", 9999)
