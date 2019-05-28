7a4122d1be0a57d5dede72ec08ca8f8a4df7366a
Joshy Cyriac <j.cyriac@gmail.com>
2019-05-28 15:22:44 +0200

    study_uid problems

--- meta/convert.py
+++ meta/convert.py
@@ -1,3 +1,6 @@
+import logging
+
+
 def convert(df):
     """Convert the query all dataframe to a download json structure"""
     data = []
@@ -8,16 +11,23 @@ def convert(df):
             "study_description": row.StudyDescription,
             "study_date": row.StudyDate,
             "accession_number": row.AccessionNumber,
-            "study_uid": row.get("StudyInstanceUID"),  # newer ones have none!
         }
         for s in row["_childDocuments_"]:
             p = parent.copy()
             # Important: Newer versions of the crawler has the StudyInstanceUID
             # on the series level (because of GRASP e.g.) Now if the series level
             # is empty it should  use the one from the study level
-            p["study_uid"] = s.get("StudyInstanceUID")
+            p["study_uid"] = s.get("StudyInstanceUID", row.get("StudyInstanceUID"))
             p["series_uid"] = s["SeriesInstanceUID"]
             p["series_description"] = s.get("SeriesDescription", "")
             p["series_number"] = s.get("SeriesNumber", 9999)
-            data.append(p)
+            if not bool(p.get("study_uid")):
+                logging.error(
+                    "Following serie not in download because of missing StudyInstanceUID, need to reindex solr"
+                )
+                logging.error(p)
+                logging.error("Accession number is: " + p.get("accession_number"))
+                print(p, s)
+            else:
+                data.append(p)
     return data
